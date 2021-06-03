c4da5f4871255f122358340c0b5d317dae8e19be
Allen Xiao <xhui.xiao@gmail.com>
2021-06-03 14:42:35 +0800

    extract createCommand function

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { AnnotationPanel } from './AnnotationPanel';
+import * as _ from 'lodash';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,35 +13,33 @@ export function activate(context: vscode.ExtensionContext) {
 	/**
 	 * open annotation panel
 	 */
-	context.subscriptions.push(
-		vscode.commands.registerCommand('annotate.openAnnotation', () => {
-			let panel = AnnotationPanel.getInstance(context);
-			panel.show();
-		})
-	);
+	createCommand(context, 'annotate.openAnnotation', () => {
+		let panel = AnnotationPanel.getInstance(context);
+		panel.show();
+	});
 
 	/**
 	 * set annotation base directory
 	 */
-	context.subscriptions.push(
-		vscode.commands.registerCommand('annotate.setAnnotationDir', () => {
-			console.log('Annotate directory set!');
-			let title = vscode.window.activeTextEditor?.document.fileName ?? "";
-			vscode.window.showInformationMessage(title);
-		})
-	);
+	createCommand(context, 'annotate.setAnnotationDir', () => {
+		console.log('Annotate directory set!');
+		let title = vscode.window.activeTextEditor?.document.fileName ?? "";
+		vscode.window.showInformationMessage(title);
+	});
 
 	/**
 	 * add annotation to selections
 	 */
-	context.subscriptions.push(
-		vscode.commands.registerCommand('annotate.addAnnotation', () => {
-			console.log('Add annotation');
-			let selection = vscode.window.activeTextEditor?.selection;
-			let text = vscode.window.activeTextEditor?.document.getText(selection);
-			vscode.window.showInformationMessage(text ?? "");
-		})
-	);
+	createCommand(context, 'annotate.addAnnotation', () => {
+		console.log('Add annotation');
+		let selection = vscode.window.activeTextEditor?.selection;
+		let text = vscode.window.activeTextEditor?.document.getText(selection);
+		vscode.window.showInformationMessage(text ?? "");
+	});
+}
+
+function createCommand(context: vscode.ExtensionContext, cmd: string, fn: (...args: any[]) => any, thisArg?: any) {
+	context.subscriptions.push(vscode.commands.registerCommand(cmd, fn));
 }
 
 // this method is called when your extension is deactivated
