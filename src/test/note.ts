8fbf5d15973d8b420c5177564edfbb083f0731f4
Allen Xiao <xhui.xiao@gmail.com>
2021-06-17 17:31:54 +0800

    update note class

--- src/test/note.ts
+++ src/test/note.ts
@@ -1,7 +1,20 @@
 import * as vscode from 'vscode';
 
 class Note {
-    static noteUri: vscode.Uri;
-    selection: vscode.Selection | undefined;
+    // root Uri of all note
+    static noteRootUri: vscode.Uri;
+    // selection from
+    readonly from: vscode.Position;
+    // selection to
+    readonly to: vscode.Position;
+    // source file annotate to
+    readonly sourceFile: vscode.Uri;
+    // markdown note content
     note: vscode.MarkdownString | undefined;
+
+    constructor(sourceFile: vscode.Uri, from: vscode.Position, to: vscode.Position) {
+        this.sourceFile = sourceFile;
+        this.from = from;
+        this.to = to;
+    }
 }
